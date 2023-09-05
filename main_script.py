1710699d680e245ea87e1d193f38bc89d9b5bcd5
TemanSv1n <temasyr.o@gmail.com>
2023-09-05 21:47:00 +0300

    fixed "too many little men in our team"
    fixed oauth problems and age restrictions

--- main_script.py
+++ main_script.py
@@ -1,29 +1,31 @@
 from moviepy.editor import *
-import pytube
+#import pytube
+import yt_dlp
 import random
 
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 def Download(link: str):
-    yt = pytube.YouTube(link,
-                        use_oauth=False,
-                        allow_oauth_cache=False
-                        )
-    yt = yt.streams.get_highest_resolution()
-    okay = False
-    try:
-        file_name = f"vid-{random.randint(1000,9999)}.mp4"
-        yt.download(output_path="downloads", filename=file_name)
-        okay = True
-        file_name = "downloads/" + file_name
-    except:
-        print("ooga booga!")
-        return "-1"
-    if okay:
-        print(file_name)
-        return file_name
-    else:
-        print("ooga booga!")
-        return "-1"
+    file_name = f"vid-{random.randint(1000, 9999)}.mp4"
+    ydl_opts = {
+        'format': 'best',  # Select the best available format
+        'outtmpl': "downloads/" + file_name,  # Output file template
+        'quiet': False,  # Suppress output messages
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        okay = False
+        try:
+            ydl.download([link])
+            okay = True
+            file_name = "downloads/" + file_name
+        except:
+            print("ooga booga!")
+            return "-1"
+        if okay:
+            print(file_name)
+            return file_name
+        else:
+            print("ooga booga!")
+            return "-1"
 
 def VideoCrop(file: str, start: float, stop: float):
     #file = "downloads/vid-4607.mp4"
